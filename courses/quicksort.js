bc94f1feb3073c9f38c39184e10bc61dfd5e2eb8
bb <bb@pop-os.localdomain>
2021-04-12 22:21:25 +0100

    qsort simula

--- courses/quicksort.js
+++ courses/quicksort.js
@@ -1,5 +1,6 @@
 let blocks = [];
 let block_width;
+let sep;
 let n_blocks = 6;
 let a7mer = "#b51919";
 let black = "black";
@@ -34,77 +35,28 @@ let blacken = () => {
         lawen(i, black);
     }
 };
-var arr = [];
-
-let sort = async function(inputArr) {
-    stop = false;
-    badd.setAttribute("disabled", "disabled");
-    bsort.setAttribute("disabled", "disabled");
-    brandom.setAttribute("disabled", "disabled");
-    let len = n_blocks - 1;
-    let last = len;
-    let swapped;
-    do {
-        swapped = false;
-        for (let i = 0; i < last; i++) {
-
-
-
-            lawen(i, a7mer);
-            lawen(i + 1, a7mer);
-            if (stop) {
-                stop = false;
-                badd.removeAttribute("disabled", "disabled");
-                bsort.removeAttribute("disabled", "disabled");
-                brandom.removeAttribute("disabled", "disabled");
-                return;
-            }
-            if (inputArr[i] > inputArr[i + 1]) {
-                swap(i, i + 1, inputArr);
-                swapId(i, i + 1);
-                swapped = true;
-            }
-            await sleep(700);
-            lawen(i, black);
-            lawen(i + 1, black);
-            if (stop) {
-                stop = false;
-                badd.removeAttribute("disabled", "disabled");
-                bsort.removeAttribute("disabled", "disabled");
-                brandom.removeAttribute("disabled", "disabled");
-
-                return;
-            }
-
-        }
-        lawen(last, green);
-        last--;
-    } while (swapped);
-    for (let i = 0; i < len + 1; i++) {
-        lawen(i, green);
-    }
-    badd.removeAttribute("disabled", "disabled");
-    bsort.removeAttribute("disabled", "disabled");
-    brandom.removeAttribute("disabled", "disabled");
-
-    return inputArr;
+let setArrow = (pos, id) => {
+    pos++;
+    arrows[id].style.marginLeft = (pos * (sep + block_width) - block_width / 2 - 1.4 * 1) + "vw";
 };
-
+var arr = [];
+let arrows = [];
 let init = () => {
 
+    sep = (25.0) / (n_blocks + 1);
     block_width = 75.0 / n_blocks;
+    arrows[0] = document.getElementById("a1");
+    arrows[1] = document.getElementById("a2");
+
     for (let i = 0; i < 12; i++) {
         arr[i] = i;
         blocks[i] = document.getElementById("block" + i);
         blocks[i].style.width = block_width + "vw";
         blocks[i].style.height = block_width + "vw";
-        let sep = (25.0) / (n_blocks + 1);
         blocks[i].style.marginLeft = ((i + 1) * sep + i * block_width) + "vw";
     }
-
-
-
-
+    setArrow(0, 0);
+    setArrow(n_blocks - 1, 1);
 };
 let randomize = () => {
     let a, b;
@@ -141,15 +93,83 @@ let check = () => {
         init();
 
     }
+    randomize();
+};
 
+let partition = async function(l, r) {
+    if (stop) return;
+    let i = l - 1;
+    let val = arr[r];
+    lawen(r, "orange");
+    for (let j = l; j < r; j++) {
+        if (stop) return;
+        if (arr[j] <= val) {
+            lawen(i + 1, a7mer);
+            lawen(j, a7mer);
+
+            swap(i + 1, j, arr);
+            swapId(i + 1, j);
+
+            await sleep(700);
+            if (stop) return;
+            lawen(i + 1, black);
+            lawen(j, black);
+            i++;
+        }
+    }
+    swap(i + 1, r, arr);
+    swapId(i + 1, r);
+    return i + 1;
 
 
 };
+
+let quicksort = async function(l, r) {
+    if (stop) return;
+
+    if (l <= r) {
+        setArrow(l, 0);
+        setArrow(r, 1);
+        await sleep(700);
+        if (stop) return;
+    }
+    if (l == r) {
+        lawen(l, green);
+    }
+    if (l >= r) return;
+    let q = await partition(l, r);
+    if (stop) return;
+
+    lawen(q, green);
+    await sleep(700);
+    await quicksort(l, q - 1);
+    await sleep(700);
+    if (stop) return;
+    await quicksort(q + 1, r);
+};
+
+
+
+
+
 var brandom = document.getElementById("randomize");
 brandom.addEventListener("click", randomize);
 
-var bsort = document.getElementById("sort");
-bsort.addEventListener("click", () => sort(arr));
+var qsort = document.getElementById("sort");
+qsort.addEventListener("click", async function() {
+    stop = false;
+    blacken();
+    badd.setAttribute("disabled", "disabled");
+    qsort.setAttribute("disabled", "disabled");
+    brandom.setAttribute("disabled", "disabled");
+
+    await quicksort(0, n_blocks - 1);
+
+    badd.removeAttribute("disabled", "disabled");
+    qsort.removeAttribute("disabled", "disabled");
+    brandom.removeAttribute("disabled", "disabled");
+
+});
 
 var badd = document.getElementById("myRange");
 badd.addEventListener("change", check);
