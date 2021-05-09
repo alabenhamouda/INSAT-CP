fb789232c5ed8a783c859f45d1e7f095f2de3990
azer <azer@pop-os.localdomain>
2021-05-09 02:04:05 +0100

    login/sinup

--- assets/login.js
+++ assets/login.js
@@ -1,7 +1,18 @@
-import $ from 'jquery/dist/jquery.min';
+/*import $ from 'jquery/dist/jquery.min';
 $(document).ready(function(){
 	AOS.init();
 	$('[data-bss-hover-animate]')
 		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bss-hover-animate')) })
 		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bss-hover-animate')) });
+});*/
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+	container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+	container.classList.remove("sign-up-mode");
 });
