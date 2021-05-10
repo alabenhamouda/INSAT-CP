b85c43f88344fd79fb69379770c05d1b202c1ddc
alabh <alabenhmouda@gmail.com>
2021-05-10 15:44:38 +0100

    style editor

--- assets/problem.js
+++ assets/problem.js
@@ -1,7 +1,9 @@
 import CodeMirror from 'codemirror/lib/codemirror'
-import   'codemirror/mode/clike/clike'
+import 'codemirror/mode/clike/clike'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/duotone-dark.css'
+import 'codemirror/theme/monokai.css'
+import 'codemirror/theme/ayu-mirage.css'
 import 'codemirror/addon/hint/show-hint'
 import 'codemirror/addon/hint/show-hint.css'
 import 'codemirror/addon/edit/closebrackets'
@@ -11,16 +13,17 @@ import ClipboardJS from 'clipboard/dist/clipboard.min';
 import $ from 'jquery'
 import 'bootstrap'
 import 'mathjax/es5/tex-svg'
-let cli=new ClipboardJS('.tocopy');
+
+let cli = new ClipboardJS('.tocopy');
 //TODO add a copied message
-$(function(){
+$(function () {
     $(".tocopy")
         .popover({
             content: "Text copied!",
             placement: "bottom",
             trigger: "manual"
         })
-        .on("click", function() {
+        .on("click", function () {
             $(this).popover("show");
             setTimeout(() => {
                 console.log(this);
@@ -28,7 +31,7 @@ $(function(){
             }, 1000)
         })
 })
-cli.on('success', function(e) {
+cli.on('success', function (e) {
     // console.info('Action:', e.action);
     // console.info('Text:', e.text);
     // console.info('Trigger:', e.trigger);
@@ -36,12 +39,16 @@ cli.on('success', function(e) {
     e.clearSelection();
 });
 if (typeof CodeMirror !== 'undefined') {
+    console.log(CodeMirror.mimeModes)
     $('.editor').each(function () {
         var editor = CodeMirror.fromTextArea(this, {
-            mode: "clike",
-            theme: "duotone-dark",
+            mode: CodeMirror.mimeModes['text/x-c++src'],
+            theme: "ayu-mirage",
             lineNumbers: true,
-            extraKeys: {"Ctrl-Space": "autocomplete"},
+            extraKeys: {
+                "Ctrl-Space":
+                    "autocomplete"
+            },
             matchBrackets: true,
             autoCloseBrackets: true,
             styleActiveLine: true
@@ -57,13 +64,13 @@ if (typeof CodeMirror !== 'undefined') {
 //         fontCache: 'global'
 //     }
 // };
-$('#btn-open-file').click(function() {
+$('#btn-open-file').click(function () {
     $('#input-open-file').trigger('click');
 });
-$('#input-open-file').on('change', function(e) {
+$('#input-open-file').on('change', function (e) {
     var fileData = e.target.files[0];
     var reader = new FileReader();
-    reader.onload = function() {
+    reader.onload = function () {
         $('.editor').data('editor').doc.setValue(reader.result);
     };
     reader.readAsText(fileData);
