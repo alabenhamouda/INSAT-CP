37ea0fee7ddd541fe28e62a7082ec8e5f0793a6a
alabh <alabenhmouda@gmail.com>
2021-05-12 03:25:57 +0100

    change url between signup and login as if they were two different routes

--- assets/login.js
+++ assets/login.js
@@ -11,10 +11,21 @@ const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
+window.onload = () => {
+    window.history.replaceState(container.className, 'first');
+}
+
+window.onpopstate = e => {
+    console.log(e.state);
+    container.className = e.state;
+}
+
 sign_up_btn.addEventListener("click", () => {
     container.classList.add("sign-up-mode");
+    window.history.pushState(container.className, "signup", "/signup");
 });
 
 sign_in_btn.addEventListener("click", () => {
     container.classList.remove("sign-up-mode");
+    window.history.pushState(container.className, "login", "/login");
 });
