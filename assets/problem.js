95c106bfaad3a6f0a41bf4471077200f181d2e24
alabh <alabenhmouda@gmail.com>
2021-06-14 10:31:50 +0100

    purify statements in problems and display html and math

--- assets/problem.js
+++ assets/problem.js
@@ -2,6 +2,15 @@ import './clipboard';
 import './highlight';
 import 'mathjax/es5/tex-svg';
 import './styles/problem.css';
+import dompurify from 'dompurify';
+import $ from 'jquery';
+
+console.log("degla")
+
+$(".purify").each(function () {
+    $(this).html(dompurify.sanitize($(this).html()));
+    console.log(dompurify.removed)
+});
 
 // MathJax = {
 //     tex: {
